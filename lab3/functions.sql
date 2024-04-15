a6102d1a4157af9322d44a8ad9f14a3f25d7da43
velnck <v.veronika.sidorko@gmail.com>
2024-04-15 21:19:44 +0300

    added tables' structure comparison

--- lab3/functions.sql
+++ lab3/functions.sql
@@ -1,22 +1,4 @@
 CREATE OR REPLACE PROCEDURE get_differences(dev_schema_name VARCHAR2, 
-                                            prod_schema_name VARCHAR2) 
-AS
-BEGIN
-    DBMS_OUTPUT.PUT_LINE("hello");
-END;
-
-
-DECLARE
-    dev_tables ALL_TABLES%ROWTYPE;
-BEGIN
-    SELECT * BULK COLLECT INTO dev_tables FROM ALL_TABLES WHERE OWNER = 'DEV';
-END;
-
-SELECT TABLE_NAME BULK COLLECT INTO prod_tables FROM ALL_TABLES WHERE OWNER = 'PROD';
-
-
-
-CREATE OR REPLACE PROCEDURE get_differences(dev_schema_name VARCHAR2, 
                                             prod_schema_name VARCHAR2)
 AS
     CURSOR dev_tables IS SELECT * FROM ALL_TABLES WHERE OWNER = dev_schema_name;
@@ -28,6 +10,10 @@ BEGIN
         FOR prod_table IN prod_tables LOOP
             IF prod_table.TABLE_NAME = dev_table.TABLE_NAME THEN
                 is_found := TRUE;
+                IF have_different_structure(dev_table.TABLE_NAME, dev_schema_name, prod_schema_name) THEN
+                    DBMS_OUTPUT.PUT_LINE(dev_table.table_name 
+                        || ' IS DIFFERENT IN STRUCTURE');
+                END IF;
                 EXIT;
             END IF;
         END LOOP; 
@@ -37,8 +23,45 @@ BEGIN
    END LOOP;
 END;
 
+
+CREATE OR REPLACE FUNCTION have_different_structure(table_name_to_check VARCHAR2, 
+                                         dev_schema VARCHAR2, 
+                                         prod_schema VARCHAR2)
+RETURN BOOLEAN
+AS
+    TYPE columns_t IS TABLE OF VARCHAR2(100);
+    dev_cols columns_t;
+    prod_cols columns_t;
+    diff_count NUMBER;
+        
+BEGIN
+    SELECT COUNT(*) INTO diff_count FROM (
+        (SELECT column_name FROM ALL_TAB_COLS
+        WHERE table_name = table_name_to_check AND owner = dev_schema
+        MINUS
+        SELECT column_name FROM ALL_TAB_COLS
+        WHERE table_name = table_name_to_check AND owner = prod_schema)
+        UNION
+        (SELECT column_name FROM ALL_TAB_COLS
+        WHERE table_name = table_name_to_check AND owner = prod_schema
+        MINUS
+        SELECT column_name FROM ALL_TAB_COLS
+        WHERE table_name = table_name_to_check AND owner = dev_schema));
+    IF diff_count = 0 
+    THEN
+        RETURN FALSE;
+    ELSE
+        RETURN TRUE; 
+    END IF;
+END;
+
+
+
+-- TEST--
 SET SERVEROUTPUT ON;
 EXEC GET_DIFFERENCES('DEV', 'PROD');
 EXEC GET_DIFFERENCES('PROD', 'DEV');
+
+
 
 
